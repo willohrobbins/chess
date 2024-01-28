2013723a8b1c386353280d9a424da956b305f557
willohrobbins <willoh_robbins@outlook.com>
2024-01-28 17:08:27 -0600

    ports

--- client.py
+++ client.py
@@ -110,5 +110,5 @@ def human_game(host, port):
 
 if __name__ == "__main__":
     host = '73.166.159.150'
-    port = 58501
+    port = 8001
     human_game(host, port)
--- server.py
+++ server.py
@@ -3,7 +3,7 @@ import threading
 import chess
 
 class ChessServer:
-    def __init__(self, host='73.166.159.150', port=58501):
+    def __init__(self, host='73.166.159.150', port=8001):
         self.board = chess.Board()
         self.sockets = []
         self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
